836ec5dfc2a1ffee352239f678018ad19b2e9f21
iii-koyanagi <koyanagi@iii-store.com>
2015-07-16 11:52:49 +0900

    x_counterをregisterに

--- src/NotFork.php
+++ src/NotFork.php
@@ -40,6 +40,7 @@ class NotFork
     public function register($counter)
     {
         $minus = $this->minus;
+        $x_counter = $this->x_counter;
 
         for ($i = 1; $i <= 5; $i++) {
             $counter[$i] = $counter[$i] - $minus[$i];
@@ -49,8 +50,8 @@ class NotFork
             if ($only_counter < 0) {
                 $counter[$keys] = 0;
             }
-
         }
+        
         return $counter;
     }
 
