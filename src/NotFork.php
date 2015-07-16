4f72c4a41d83ac2d37636f79aa9ec61451fc69d7
iii-koyanagi <koyanagi@iii-store.com>
2015-07-16 15:19:08 +0900

    x_memory2

--- src/NotFork.php
+++ src/NotFork.php
@@ -87,7 +87,6 @@ class NotFork
             if ($only_data === 'x') {
                 if ($x_memory[$key_arr[0]] === 0) {
                     $x_memory[$key_arr[0]] = $counter[$key_arr[0]];
-
                     $this->x_memory = $x_memory;
                 }
                 $x_counter[$key_arr[0]] += 1;
@@ -102,6 +101,10 @@ class NotFork
             $shift_key_arr = array_shift($sort_key_arr);
 
             if ($only_data === 'x') {
+                if ($x_memory[$shift_key_arr] === 0) {
+                    $x_memory[$shift_key_arr] = $counter[$shift_key_arr];
+                    $this->x_memory = $x_memory;
+                }
                 $x_counter[$shift_key_arr] += 1;
             }
 
